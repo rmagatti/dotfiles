b2718e5c7dc8888ff198d5e705e25986f2694d44
Ronnie Magatti <ronniemagatti@gmail.com>
2022-09-09 16:21:29 -0600

    Add yamlls

--- nvim/lua/rmagatti/lsp/init.lua
+++ nvim/lua/rmagatti/lsp/init.lua
@@ -3,5 +3,6 @@ local common_on_attach = require("rmagatti.lsp.lsp-common").common_on_attach
 require("nvim-lsp-installer").setup {}
 require("rmagatti.lsp.lsp-lua").setup()
 require("rmagatti.lsp.lsp-typescript").setup()
+require("rmagatti.lsp.lsp-yaml").setup()
 
 require("rmagatti.lsp.null-ls").setup(common_on_attach)
