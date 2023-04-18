2f476d086f1bfd8ef2cb7683f8e8f973e02124f3
Ronnie Magatti <ronniemagatti@gmail.com>
2023-04-17 19:23:47 -0600

    Add python lsp config

---
+++ nvim/lua/rmagatti/lsp/lsp-python.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.setup = function()
+  local capabilities = require("rmagatti.lsp.lsp-common").capabilities
+  local common_on_attach = require("rmagatti.lsp.lsp-common").common_on_attach
+
+  local opts = {
+    on_attach = common_on_attach,
+    capabilities = capabilities,
+  }
+
+  require("lspconfig").pylsp.setup(vim.tbl_deep_extend("force", opts, {}))
+end
+
+return M
