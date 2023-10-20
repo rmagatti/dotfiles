ca625ea43d366eea8455bca0c59d1106321e604f
Ronnie Magatti <ronniemagatti@Ronnies-MacBook-Pro.local>
2023-10-20 01:58:58 -0600

    New computer MBP16

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -226,7 +226,7 @@ require("lazy").setup({
   },
   {
     "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     dependencies = { "nvim-telescope/telescope.nvim" },
   },
   {
