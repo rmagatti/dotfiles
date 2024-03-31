090931d7d83d4f80513e577cd5531a08872d9c75
Ronnie Magatti <ronniemagatti@gmail.com>
2024-03-31 00:04:22 -0600

    chore: Update plugin dependencies and configurations
    - Added support for `production` environment and `unstringify` function in [zsh/zshrc]
    - Modified the value of `CC` environment variable in [zsh/zshrc]
    - Added `SUPABASE_URL` and `SUPABASE_API_KEY` environment variables in [zsh/zshrc]
    - Fixed a typo in the require statement and made changes to plugin setup in [nvim/lua/plugins.lua]

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -25,20 +25,20 @@ local function add_if_vscode(should_add, plugin)
 end
 
 require("lazy").setup({
-  add_if_vscode(true, { "tpope/vim-eunuch" }),
-  add_if_vscode(true, {
+  { "tpope/vim-eunuch" },
+  {
     "tpope/vim-commentary",
     keys = {
       { "gc" },
       { "gc", mode = "v" },
     }
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "tpope/vim-abolish",
     event = "VeryLazy",
-  }),
-  add_if_vscode(true, { "JoosepAlviste/nvim-ts-context-commentstring" }),
-  add_if_vscode(true, {
+  },
+  { "JoosepAlviste/nvim-ts-context-commentstring" },
+  {
     "tpope/vim-surround",
     keys = {
       { "cs" },
@@ -46,30 +46,30 @@ require("lazy").setup({
       { "S", mode = "x" },
       { "ys" },
     },
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "tpope/vim-repeat",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "akinsho/toggleterm.nvim",
     version = "*",
     event = "VeryLazy",
     config = function()
       require "rmagatti.toggleterm"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "mbbill/undotree",
     config = function()
       vim.cmd [[nnoremap <leader>u :UndotreeShow<CR>]]
     end,
     keys = "<leader>u",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "jremmen/vim-ripgrep",
     cmd = { "Rg" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "mhinz/vim-grepper",
     lazy = false,
     init = function()
@@ -78,13 +78,13 @@ require("lazy").setup({
         xmap gs <plug>(GrepperOperator)
       ]]
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-tree/nvim-web-devicons",
     config = function()
       require "rmagatti.nvim-web-devicons"
     end,
-  }),
+  },
   -- add_if_vscode(false, {
   --   "vim-test/vim-test",
   --   config = function()
@@ -108,21 +108,21 @@ require("lazy").setup({
   --     { "<leader>tdv" },
   --   },
   -- }),
-  add_if_vscode(false, {
+  {
     "windwp/nvim-autopairs",
     config = function()
       require "rmagatti.nvim-autopairs"
     end,
     event = "InsertEnter",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "stevearc/oil.nvim",
     lazy = false,
     config = function()
       require("rmagatti.oil").setup()
     end,
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "ggandor/leap.nvim",
     config = function()
       require("leap").create_default_mappings()
@@ -135,15 +135,15 @@ require("lazy").setup({
       { "F" },
       { "<localleader>s", mode = { "n", "x", "o" } }
     },
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "tommcdo/vim-exchange",
     keys = {
       { "cx" },
       { "X", mode = "x" },
     },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "glacambre/firenvim",
     lazy = false,
     build = function()
@@ -152,16 +152,16 @@ require("lazy").setup({
     config = function()
       require "rmagatti.firenvim"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "mg979/vim-visual-multi",
     branch = "master",
     keys = "<C-n>",
     config = function()
       vim.cmd [[let g:VM_custom_motions  = {'h': 'j', 'j': 'k', 'k':'l', 'l':';'}]]
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "neovim/nvim-lspconfig",
     dependencies = {
       "j-hui/fidget.nvim"
@@ -170,21 +170,21 @@ require("lazy").setup({
     config = function()
       require "rmagatti.lsp"
     end,
-  }),
-  add_if_vscode(false, "williamboman/mason.nvim"),
-  add_if_vscode(false, "williamboman/mason-lspconfig.nvim"),
-  add_if_vscode(false, {
+  },
+  { "williamboman/mason.nvim" },
+  { "williamboman/mason-lspconfig.nvim" },
+  {
     "j-hui/fidget.nvim",
     tag = "legacy",
     event = "LspAttach",
     config = function()
       require "rmagatti.fidget"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "ray-x/lsp_signature.nvim",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "hrsh7th/nvim-cmp",
     event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
@@ -197,8 +197,8 @@ require("lazy").setup({
     config = function()
       require "rmagatti.nvim-cmp"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "L3MON4D3/LuaSnip",
     -- follow latest release.
     version = "v1.*",
@@ -207,15 +207,15 @@ require("lazy").setup({
     config = function()
       require "rmagatti.luasnip"
     end,
-  }),
-  add_if_vscode(false, "rafamadriz/friendly-snippets"),
-  add_if_vscode(true, { "saadparwaiz1/cmp_luasnip" }),
-  add_if_vscode(false, { "hrsh7th/cmp-nvim-lsp" }),
-  add_if_vscode(false, { "hrsh7th/cmp-buffer" }),
-  add_if_vscode(false, { "hrsh7th/cmp-path" }),
-  add_if_vscode(false, { "hrsh7th/cmp-cmdline" }),
-  add_if_vscode(false, { "lukas-reineke/cmp-rg" }),
-  add_if_vscode(false, {
+  },
+  { "rafamadriz/friendly-snippets" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "lukas-reineke/cmp-rg" },
+  {
     "David-Kunz/cmp-npm",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -224,29 +224,29 @@ require("lazy").setup({
     config = function()
       require("cmp-npm").setup {}
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "onsails/lspkind-nvim",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "folke/neodev.nvim"
-  }),
-  add_if_vscode(false, { "nvim-lua/plenary.nvim" }),
-  add_if_vscode(false, {
+  },
+  { "nvim-lua/plenary.nvim" },
+  {
     "nvim-telescope/telescope.nvim",
     lazy = false,
     dependencies = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
     config = function()
       require "rmagatti.telescope"
     end
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-telescope/telescope-fzf-native.nvim",
     build =
     'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
     dependencies = { "nvim-telescope/telescope.nvim" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "tpope/vim-fugitive",
     config = function()
       require("rmagatti.fugitive").setup()
@@ -255,17 +255,15 @@ require("lazy").setup({
       { "<leader>gb" },
       { "<leader>gd" },
     }
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "tpope/vim-rhubarb",
     dependencies = "vim-fugitive",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "NeogitOrg/neogit",
-    cmd = { "Neogit" },
-    keys = {
-      { "<leader>gg" },
-    },
+    -- dev = true,
+    lazy = false,
     dependencies = {
       "nvim-lua/plenary.nvim",         -- required
       "nvim-telescope/telescope.nvim", -- optional
@@ -274,15 +272,15 @@ require("lazy").setup({
     config = function()
       require("rmagatti.neogit")
     end
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "sindrets/diffview.nvim",
     config = function()
       require "rmagatti.diffview"
     end,
     cmd = { "DiffviewOpen" },
     keys = "<leader>ddo",
-  }),
+  },
   -- add_if_vscode(false, {
   --   "folke/tokyonight.nvim",
   --   lazy = false, -- make sure we load this during startup
@@ -291,85 +289,85 @@ require("lazy").setup({
   --     require "rmagatti.tokyonight"
   --   end,
   -- }),
-  add_if_vscode(false, {
+  {
     "rebelot/kanagawa.nvim",
     lazy = false, -- make sure we load this during startup
     priority = 1000,
     config = function()
       require "rmagatti.kanagawa"
     end,
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       require "rmagatti.treesitter"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-treesitter/nvim-treesitter-context",
     dependencies = "nvim-treesitter/nvim-treesitter-context",
     event = { "BufReadPost" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-treesitter/playground",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     cmd = "TSPlaygroundToggle",
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "p00f/nvim-ts-rainbow",
     event = "BufReadPost",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-treesitter/nvim-treesitter-textobjects",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     event = { "BufReadPost" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "RRethy/nvim-treesitter-textsubjects",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     event = { "BufReadPost" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "lewis6991/gitsigns.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require "rmagatti.gitsigns"
     end,
     event = "BufRead",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "junegunn/fzf",
     build = function()
       vim.fn["fzf#install"]()
     end,
     event = { "BufReadPost" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "junegunn/fzf.vim",
     dependencies = { "kevinhwang91/nvim-bqf", "junegunn/fzf" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     dependencies = { { "nvim-tree/nvim-web-devicons", optional = true } },
     config = function()
       require "rmagatti.lualine"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "hashivim/vim-terraform",
     ft = { "terraform" },
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "wellle/targets.vim",
     event = { "BufReadPost" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  }),
+  },
   -- add_if_vscode(false, {
   --   'mfussenegger/nvim-lint',
   --   event = "BufReadPost",
@@ -377,55 +375,55 @@ require("lazy").setup({
   --     require('rmagatti.nvim-lint').setup()
   --   end
   -- }),
-  add_if_vscode(false, {
+  {
     "heavenshell/vim-jsdoc",
     ft = { "javascript", "javascript.jsx", "typescript" },
     cmd = "JsDoc",
     build = "make install",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "kevinhwang91/nvim-bqf",
     dependencies = { { "junegunn/fzf", optional = true }, { "junegunn/fzf.vim", optional = true } },
     ft = { "qf" },
     config = function()
       require "rmagatti.bqf"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "junegunn/vim-easy-align",
     config = function()
       require "rmagatti.easyalign"
     end,
     cmd = { "EasyAlign" },
-  }),
+  },
   -- DAP
-  add_if_vscode(false, {
+  {
     "mfussenegger/nvim-dap",
     config = function()
       require("telescope").load_extension "dap"
       require "rmagatti.dap"
     end,
     keys = { { "<leader>db" }, { "<leader>dB" } },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "theHamsta/nvim-dap-virtual-text",
     dependencies = "mfussenegger/nvim-dap",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-telescope/telescope-dap.nvim",
     dependencies = { "mfussenegger/nvim-dap", "nvim-telescope/telescope.nvim" },
     config = function()
       require("telescope").load_extension "dap"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "rcarriga/nvim-dap-ui",
     dependencies = { "mfussenegger/nvim-dap" },
     config = function()
       require "rmagatti.dap.dap-ui"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     init = function()
@@ -435,20 +433,20 @@ require("lazy").setup({
     config = function()
       require "rmagatti.which-key"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "lukas-reineke/indent-blankline.nvim",
     event = { "InsertEnter" },
     init = function()
       vim.g.indent_blankline_buftype_exclude = { "terminal" }
       vim.g.indent_blankline_filetype_exclude = { "toggleterm" }
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "AndrewRadev/bufferize.vim",
     cmd = { "Bufferize" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "folke/zen-mode.nvim",
     config = function()
       require("zen-mode").setup {
@@ -467,8 +465,8 @@ require("lazy").setup({
       ]]
     end,
     keys = "<leader>zz",
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "AckslD/nvim-neoclip.lua",
     -- dependencies = { "tami5/sqlite.lua", },
     event = "BufReadPost",
@@ -481,15 +479,15 @@ require("lazy").setup({
     keys = {
       { "<leader>y" },
     },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "rmagatti/auto-session",
     config = function()
       require "rmagatti.auto-session"
     end,
     dev = true
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "rmagatti/alternate-toggler",
     config = function()
       require("alternate-toggler").setup {
@@ -504,8 +502,8 @@ require("lazy").setup({
       )
     end,
     event = { "BufReadPost" },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "rmagatti/goto-preview",
     dev = true,
     config = function()
@@ -518,31 +516,31 @@ require("lazy").setup({
       { "gP" },
       { "L" },
     },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "rmagatti/igs.nvim",
     event = { "BufReadPost" },
     dev = true,
     config = function()
       require "rmagatti.igs"
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "zbirenbaum/copilot.lua",
     event = { "InsertEnter" },
     config = function()
       require("rmagatti.copilot").setup()
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "zbirenbaum/copilot-cmp",
     dependencies = { "zbirenbaum/copilot.lua" },
     event = { "InsertEnter", "LspAttach" },
     config = function()
       require("rmagatti.copilot-cmp").setup()
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "eandrju/cellular-automaton.nvim",
     config = function()
       vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>", { noremap = true })
@@ -550,8 +548,8 @@ require("lazy").setup({
     keys = {
       { "<leader>fml" },
     },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "sourcegraph/sg.nvim",
     config = require("rmagatti.sg").setup,
     dependencies = {
@@ -561,19 +559,19 @@ require("lazy").setup({
     keys = {
       { "<leader>sg" },
     },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "wakatime/vim-wakatime",
     event = "VeryLazy"
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "stevearc/dressing.nvim",
     event = "VeryLazy",
     config = function()
       require("rmagatti.dressing").setup()
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-neorg/neorg",
     ft = "norg",
     config = function()
@@ -585,21 +583,21 @@ require("lazy").setup({
     keys = {
       { "<leader>nn" }
     }
-  }),
-  add_if_vscode(true, {
+  },
+  {
     "rmagatti/gx-extended.nvim",
     event = "VeryLazy",
     config = function()
       require("rmagatti.gx-extended").setup()
     end,
     dev = true
-  }),
-  add_if_vscode(false, {
+  },
+  {
     'mrcjkb/rustaceanvim',
     version = '^4', -- Recommended
     ft = { 'rust' },
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
       { "nvim-lua/plenary.nvim" },
@@ -608,8 +606,8 @@ require("lazy").setup({
     config = function()
       require("rmagatti.refactoring").setup()
     end,
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
     lazy = true,
@@ -642,14 +640,14 @@ require("lazy").setup({
       }
     },
 
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "m00qek/baleia.nvim",
     config = function()
       require("baleia").setup {}
     end
-  }),
-  add_if_vscode(false, {
+  },
+  {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-lua/plenary.nvim",
@@ -659,5 +657,5 @@ require("lazy").setup({
     config = function()
       require("rmagatti.neotest").setup()
     end,
-  })
+  }
 }, { defaults = { lazy = true }, dev = { path = "~/Projects" } })
